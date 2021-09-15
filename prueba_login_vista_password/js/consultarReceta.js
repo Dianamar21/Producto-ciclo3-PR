6ce40608a1fb8d196567d2107a742abf557ba243
Leo <ing.victorerazo@hotmail.com>
2021-09-15 01:32:37 -0500

    ajuste JS de consulta con boton e ingredientes

--- prueba_login_vista_password/js/consultarReceta.js
+++ prueba_login_vista_password/js/consultarReceta.js
@@ -3,10 +3,21 @@ function mostrarEnPantalla(){
     //const salida= document.getElementById("salida");
     //const salida2= document.getElementById("salida");
     const salida3= document.getElementById("salida");
+    
 
     let resultado = "";
+    //var datos_receta = "<ul>ingrediente 1</ul> <ul>ingrediente 2</ul><br>"
+    var botonzaso = "<div class='d-grid gap-2'><button type='button ' class='btn btn-primary' onclick='mostrarEnPantalla()'><b>Detalles</b></button></div>";
+    
+    
+                
+    var datos_receta = "ingrediente 1 <br> ingrediente 2<br>"+botonzaso;
     for(let i=0; i<6; i++){
-        resultado += "<div class ='col'>" +"<b>Receta: </b>"+inputName.value+ " " +(i+1)+ "</div>";
+        resultado += "<div class ='col'>" 
+                    +"<b>Receta: </b>"
+                    +inputName.value+ " " +(i+1)+
+                    "<br>"
+                    +datos_receta+"<br></div>";
         
     }
     
@@ -15,8 +26,9 @@ function mostrarEnPantalla(){
     //salida.innerHTML = "<h3>"+inputName.value+"</h3>";
     //salida2.innerHTML = "<div class ='row row-cols-3'>"+inputName.value+"</div>";
     //salida3.innerHTML = "<div class ='row row-col-lg-3 row-col-md-3 row-col-sm-1 row-cols-3'>"+resultado+"</div>";
+    salida3.innerHTML = "<div class ='row row-col-xs-1 row-col-sm-1'>"+resultado+"</div>";
+    
     //salida3.innerHTML = "<div class ='row row-cols-3'>"+resultado+"</div>";
-    salida3.innerHTML = "<div class ='row row-cols-3'>"+resultado+"</div>";
 
     console.log(inputName.value);
     inputName.value = "";
