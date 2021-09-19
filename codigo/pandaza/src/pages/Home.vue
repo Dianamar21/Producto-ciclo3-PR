7b40128cf626b824b3edeb8310d18e5fb8f01992
Leo <ing.victorerazo@hotmail.com>
2021-09-19 16:52:31 -0500

    banner de receta puesto

--- codigo/pandaza/src/pages/Home.vue
+++ codigo/pandaza/src/pages/Home.vue
@@ -3,11 +3,12 @@
   <div>
   
       <v-img
-      lazy-src="https://picsum.photos/id/11/10/6"
+      lazy-src="../assets/banner_2.jpg"
       max-height="300"
       max-width="1400"
-      src="https://picsum.photos/id/11/500/300"
+      src="../assets/banner_2.jpg"
     ></v-img>
+      <!--ANTES: lazy-src="https://picsum.photos/id/11/10/6"-->
       
       <br>
   
