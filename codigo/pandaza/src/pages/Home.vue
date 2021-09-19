84e0dbbad5a86382fb9736d1fabcc87b8e165528
Leo <ing.victorerazo@hotmail.com>
2021-09-19 16:43:26 -0500

    pequeño banner en home

--- codigo/pandaza/src/pages/Home.vue
+++ codigo/pandaza/src/pages/Home.vue
@@ -1,6 +1,16 @@
 <template>
   
   <div>
+  
+      <v-img
+      lazy-src="https://picsum.photos/id/11/10/6"
+      max-height="300"
+      max-width="1400"
+      src="https://picsum.photos/id/11/500/300"
+    ></v-img>
+      
+      <br>
+  
   <!--   DATOS DE TITULO ANTES DE CONSULTAR RECETA      -->
     <h1>Bienvenido a Pandaza</h1>
     <h4>El recetario colaborativo mas grande del mundo</h4>
