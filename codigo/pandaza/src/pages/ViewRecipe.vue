addcdba375a5387b7b6d0e3e0c5dde0eafbd85f8
Leo <ing.victorerazo@hotmail.com>
2021-09-19 18:31:35 -0500

    ver receta actualizado

--- codigo/pandaza/src/pages/ViewRecipe.vue
+++ codigo/pandaza/src/pages/ViewRecipe.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
     <h1>Ver Receta</h1>
+    <br>
+  <div>
+
+    <v-simple-table >
+    <!-- <v-simple-table dark> -->
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Calories
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in desserts"
+          :key="item.name"
+        >
+          <td>{{ item.name }}</td>
+          <td>{{ item.calories }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+
+
+  </div>
+  
+  
   </div>
 </template>
 
