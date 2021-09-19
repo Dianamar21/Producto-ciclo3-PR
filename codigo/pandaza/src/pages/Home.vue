281f25d44a25b83f8c6605e663574244156e9a02
Leo <ing.victorerazo@hotmail.com>
2021-09-19 17:45:53 -0500

    se agrego login en home

--- codigo/pandaza/src/pages/Home.vue
+++ codigo/pandaza/src/pages/Home.vue
@@ -2,6 +2,58 @@
   
   <div>
   
+  <div>
+    <!--   CAJON EXCLUSIVO LOGIN      -->
+    <h4>LOGIN</h4>
+    
+    <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="Usuario"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+      
+
+      <v-col
+          cols="12"
+          md="4"
+        >
+      <v-btn
+      depressed
+      color="primary"
+    >
+      LOGIN
+    </v-btn>
+      </v-col>
+    </v-row>
+
+
+    </v-container>
+  </v-form>
+
+  </div>
+      <!--   BANNERRRRRRRRRRRRRRRRRRRRRRRRRRRR      -->
       <v-img
       lazy-src="../assets/banner_2.jpg"
       max-height="300"
