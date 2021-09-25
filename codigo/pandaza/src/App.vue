5ad7e05bdc1a2cba77076268822bfc8e0db133ca
gem243 <gem243@gmail.com>
2021-09-24 23:42:35 -0400

    Cambios

--- codigo/pandaza/src/App.vue
+++ codigo/pandaza/src/App.vue
@@ -1,24 +1,23 @@
   
 <template>
   <!--<v-app id="inspire">-->
-    <!-- If using vue-router -->
-    <!--<router-view></router-view>-->
+  <!-- If using vue-router -->
+  <!--<router-view></router-view>-->
   <!--</v-app>-->
 
-   <!--SECCION MENU LATERAL-->
+  <!--SECCION MENU LATERAL-->
   <v-app>
-  <v-navigation-drawer class="deep-purple accent-4"
-      v-model="drawer" src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg" 
+    <v-navigation-drawer
+      class="deep-purple accent-4"
+      v-model="drawer"
+      src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
       dark
-      app >
+      app
+    >
       <!--ANTES: v-model="drawer" src="https://picsum.photos/1920/1080?random"-->
-    <!-- iconos y texto en menu lateral -->
-    <v-list>
-        <v-list-item
-          v-for="([icon, text], i) in items"
-          :key="i"
-          link
-        >
+      <!-- iconos y texto en menu lateral -->
+      <v-list>
+        <v-list-item v-for="([icon, text], i) in items" :key="i" link>
           <v-list-item-icon>
             <v-icon>{{ icon }}</v-icon>
           </v-list-item-icon>
@@ -28,93 +27,84 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
-  </v-navigation-drawer>
+    </v-navigation-drawer>
 
-  <v-app-bar
-      color="deep-purple accent-4"  dark app
+    <v-app-bar
+      color="deep-purple accent-4"
+      dark
+      app
       src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
-      >
-      <!-- ANTES:  src="https://picsum.photos/1920/1080?random"-->  
+    >
+      <!-- ANTES:  src="https://picsum.photos/1920/1080?random"-->
 
       <v-app-bar-nav-icon @click="drawer = !drawer">
         <!-- otra opcion: v-on:click  -->
-        
       </v-app-bar-nav-icon>
-      
-      <v-toolbar-title>{{title}}</v-toolbar-title>
+
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-
-      <v-btn icon>
+      <!-- componente se muestra solo si el usuario esta autenticado o no -->
+      <v-btn icon :v-show="isAuthenticated">
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
 
-      <v-menu
-        left
-        bottom
-      >
+      <v-menu left bottom>
         <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
+          <v-btn icon v-bind="attrs" v-on="on">
             <v-icon>mdi-dots-vertical</v-icon>
           </v-btn>
         </template>
 
         <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            @click="() => {}"
-          >
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
             <v-list-item-title>Option {{ n }}</v-list-item-title>
           </v-list-item>
         </v-list>
       </v-menu>
     </v-app-bar>
 
-  <!-- Sizes your content based upon application components -->
-  <v-main>
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
 
-    <!-- Provides the application the proper gutter -->
-    <v-container fluid>
-
-      <!-- If using vue-router -->
-      <router-view></router-view>
-    </v-container>
-  </v-main>
-
-  <v-footer app>
-    <!-- -->
-  </v-footer>
-</v-app>
-
-
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-
 //import HelloWorld from './components/HelloWorld';
 
- export default {
-    //
-    data(){
-      return{
-        title: "Pandaza",
-        drawer: false,
-        items: [
-        ['mdi-email', 'Inbox'],
-        ['mdi-account-supervisor-circle', 'Supervisors'],
-        ['mdi-clock-start', 'Clock-in'],
-        ],
-      }
-    }, method:{
-      
-    }
-  }
+export default {
+  //
+  data() {
+    return {
+      title: "Pandaza",
+      drawer: false,
+      items: [
+        ["mdi-email", "Inbox"],
+        ["mdi-account-supervisor-circle", "Supervisors"],
+        ["mdi-clock-start", "Clock-in"],
+      ],
+      // sessionToken: loadToken(),
+      // isAuthenticated: !!sessionToken,
+    };
+  },
+  method: {
+    loadToken() {
+      const token = localStorage.getItem("pandaza-token");
+      return token;
+    },
+  },
+};
 </script>
 
 <style>
--- codigo/pandaza/src/pages/AddRecipe.vue
+++ codigo/pandaza/src/pages/AddRecipe.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="transition-swing text-h3 mb--1">Editar Receta</div>
+    <div class="transition-swing text-h3 mb--1">Agregar Receta</div>
     <v-form>
       <v-container>
         <v-row>
@@ -14,13 +14,27 @@
         </v-row>
         <v-row>
           <v-col cols="12">
-              <v-file-input
-    accept="image/*"
-    label="File input"
-  ></v-file-input>
+            <v-file-input accept="image/*" label="File input"></v-file-input>
           </v-col>
         </v-row>
-        <v-row v-for="ingridient in recipe.ingridients" :key="ingridient.id">
+        <v-row>
+          <v-col cols="5">
+            <v-text-field
+              outlined
+              label="Nombre Ingrediente:"
+              placeholder="Arroz"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="5">
+            <v-text-field outlined label="Cantidad:" placeholder="1 lb"></v-text-field>
+          </v-col>
+          <v-col cols="1">
+            <v-btn class="mx-2" fab dark color="indigo">
+              <v-icon dark> mdi-plus </v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+        <!-- <v-row v-for="ingridient in recipe.ingridients" :key="ingridient.id">
           <v-col cols="6">
             <v-subheader v-text="ingridient.name" />
           </v-col>
@@ -30,30 +44,14 @@
               v-text="ingridient.quantity"
               suffix="lbs"
             ></v-text-field>
-                <v-btn
-          class="mx-2"
-          fab
-          dark
-          small
-          color="primary"
-        >
-          <v-icon dark>
-            mdi-minus
-          </v-icon>
-        </v-btn>
-          </v-col>
-        </v-row>
-         <v-btn
-              class="mx-2"
-              fab
-              dark
-              small
-              color="primary"
-            >
-              <v-icon dark>
-                mdi-plus
-              </v-icon>
+            <v-btn class="mx-2" fab dark small color="primary">
+              <v-icon dark> mdi-minus </v-icon>
             </v-btn>
+          </v-col>
+        </v-row> -->
+        <!-- <v-btn class="mx-2" fab dark small color="primary">
+          <v-icon dark>mdi-plus</v-icon>
+        </v-btn> -->
         <v-row>
           <v-col cols="12">
             <v-textarea
@@ -64,29 +62,21 @@
             />
           </v-col>
         </v-row>
-        <v-row
-          align="center"
-          justify="space-around"
-        >
-          <v-btn depressed
-          color="primary">
-            Guardar
-          </v-btn>
+        <v-row align="center" justify="space-around">
+          <v-btn depressed color="primary"> Guardar </v-btn>
         </v-row>
       </v-container>
     </v-form>
   </div>
-
 </template>
 
 <script>
-import RecepiesData from "../dummy/recipes.json";
-
 export default {
   components: {},
   data() {
     return {
-      recipe: RecepiesData[0],
+      recipe: {},
+      ingridients: [],
     };
   },
 };
