2958d33a48a29a3363698fd9cdb6444991253528
gem243 <gem243@gmail.com>
2021-09-28 22:18:19 -0400

    Validacion del login, con password visible

--- codigo/pandaza/src/components/Login.vue
+++ codigo/pandaza/src/components/Login.vue
@@ -1,4 +1,38 @@
-<template></template>
+<template>
+  <div>
+    <!--   CAJON EXCLUSIVO LOGIN      -->
+    <h4>LOGIN</h4>
+
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field
+              :rules="[rules.required, rules.email]"
+              label="E-mail"
+              required
+            />
+          </v-col>
+
+          <v-col cols="12" md="4">
+            <v-text-field
+              label="Password"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[rules.required, rules.min]"
+              :type="showPassword ? 'text' : 'password'"
+              required
+              @click:append="showPassword = !showPassword"
+            />
+          </v-col>
+
+          <v-col cols="12" md="4">
+            <v-btn depressed color="primary"> LOGIN </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </div>
+</template>
 
 <script>
 import axios from "axios";
@@ -10,8 +44,23 @@ export default {
         "http://localhost:3000/api/session/login",
         { username: "test", password: "test123" }
       );
-      localStorage.setItem('pandaza-token', token);
+      localStorage.setItem("pandaza-token", token);
     },
+  },
+  data() {
+    return {
+      showPassword: false,
+      username: "",
+      email: "",
+      rules: {
+        required: (value) => !!value || "Campo requerido.",
+        email: (value) => {
+          const pattern =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Correo invalido.";
+        },
+      },
+    };
   },
 };
 </script>
--- codigo/pandaza/src/pages/Home.vue
+++ codigo/pandaza/src/pages/Home.vue
@@ -1,89 +1,36 @@
 <template>
-  
   <div>
-  
-  <div>
-    <!--   CAJON EXCLUSIVO LOGIN      -->
-    <h4>LOGIN</h4>
-    
-    <v-form v-model="valid">
-    <v-container>
-      <v-row>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="firstname"
-            :rules="nameRules"
-            :counter="10"
-            label="Usuario"
-            required
-          ></v-text-field>
-        </v-col>
+    <PandazaLogin />
 
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
-        </v-col>
-      
-
-      <v-col
-          cols="12"
-          md="4"
-        >
-      <v-btn
-      depressed
-      color="primary"
-    >
-      LOGIN
-    </v-btn>
-      </v-col>
-    </v-row>
-
-
-    </v-container>
-  </v-form>
-
-  </div>
-      <!--   BANNERRRRRRRRRRRRRRRRRRRRRRRRRRRR      -->
-      <v-img
+    <!--   BANNERRRRRRRRRRRRRRRRRRRRRRRRRRRR      -->
+    <v-img
       lazy-src="../assets/banner_2.jpg"
       max-height="300"
       max-width="1400"
       src="../assets/banner_2.jpg"
     ></v-img>
-      <!--ANTES:src="https://picsum.photos/id/11/500/300"-->
-      <!--ANTES: lazy-src="https://picsum.photos/id/11/10/6"-->
-      
-      <br>
-  
-  <!--   DATOS DE TITULO ANTES DE CONSULTAR RECETA      -->
+    <!--ANTES:src="https://picsum.photos/id/11/500/300"-->
+    <!--ANTES: lazy-src="https://picsum.photos/id/11/10/6"-->
+
+    <br />
+
+    <!--   DATOS DE TITULO ANTES DE CONSULTAR RECETA      -->
     <h1>Bienvenido a Pandaza</h1>
     <h4>El recetario colaborativo mas grande del mundo</h4>
-    <recipeConsult></recipeConsult>
-  </div> 
-
+    <RecipeConsult></RecipeConsult>
+  </div>
 </template>
 
 
 
 <script>
-
-import recipeConsult from '../components/recipeConsult.vue'
-
+import PandazaLogin from "../components/Login.vue";
+import RecipeConsult from "../components/recipeConsult.vue";
 
 export default {
   components: {
-    recipeConsult
+    RecipeConsult,
+    PandazaLogin,
   },
-   
 };
 </script>
