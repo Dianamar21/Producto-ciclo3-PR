11581a1954a92968b2c9dc1841a864d55b907dda
Dianamar21 <84254354+Dianamar21@users.noreply.github.com>
2021-10-04 00:28:42 -0500

    Se eliminó el anterior archivo de backend

--- codigo/pandaza-backend/app/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const sessionRoute = require('./sessionRoutes');
-
-router.use('/session', sessionRoute);
-
-module.exports = router;
